df36b5c9ac68b9e6e313d4f4762501b37bb27c34
avinashmanchala <am21869n@pace.edu>
2024-10-15 22:48:19 -0400

    got to many renders

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -2,18 +2,9 @@ import { useState } from 'react';
 
 
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue('X');
-  }
-
+function Square({ value, onSquareClick }) {
   return (
-    <button
-      className="square"
-      onClick={handleClick}
-    >
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
@@ -21,23 +12,16 @@ function Square() {
 
 // ...
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
-    <>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-    </>
-  );
+    <Square value={squares[0]} onSquareClick={handleClick(0)} />
+    // ...
+  )
 }
